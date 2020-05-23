34f2e38d2acb3bfe8ca085b30b4806f1527db897
Alex Sartori <alex.sartori1997@gmail.com>
2020-05-24 01:08:09 +0200

    Create some skeleton code

--- src/main/java/main/QTOB.java
+++ src/main/java/main/QTOB.java
@@ -1,18 +1,46 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package main;
+import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.io.IOException;
+
 
 /**
  *
  * @author alex
  */
 public class QTOB {
-  
+    final static int N_CLIENTS = 10;
+    final static int N_REPLICAS = 10;
+    final static int MAX_NWK_DELAY_MS = 200;
+    final static int CRASH_TIMEOUT_MS = MAX_NWK_DELAY_MS + 50;
+    
+    
     public static void main(String[] args) {
-        System.out.println("Hello Akka!");
+        final ActorSystem akka = ActorSystem.create("QTOB");
+        
+        List<ActorRef> clients = new ArrayList<>();
+        for (int i=0; i < N_CLIENTS; i++) {
+            // Add clients to ArrayList
+        }
+        
+        List<ActorRef> replicas = new ArrayList<>();
+        for (int i=0; i < N_CLIENTS; i++) {
+            // Add replicas to ArrayList
+        }
+        
+        
+        // Handle termination
+        try {
+          System.out.println(">>> Press ENTER to exit <<<");
+          System.in.read();
+        }
+        catch (IOException ioe) {
+            System.out.println("Exiting...");
+        }
+        akka.terminate();
     }
     
 }
---
+++ src/main/java/main/ReplicaActor.java
@@ -0,0 +1,18 @@
+package main;
+import akka.actor.ActorRef;
+import akka.actor.AbstractActor;
+import akka.actor.Cancellable;
+import akka.actor.Props;
+
+/**
+ *
+ * @author alex
+ */
+public class ReplicaActor extends AbstractActor {
+
+    @Override
+    public Receive createReceive() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+}
