955eb5604b98664e93ba8236f9fc151731203a96
Alex Sartori <alex.sartori1997@gmail.com>
2020-07-28 00:32:58 +0200

    Add UpdateList.java

--- src/main/java/main/Update.java
+++ src/main/java/main/Update.java
@@ -12,4 +12,12 @@ public class Update {
         this.id = id;
         this.value = v;
     }
+    
+    public boolean happensBefore(Update other) {
+        return id.happensBefore(other.id);
+    }
+    
+    public boolean happensAfter(Update other) {
+        return id.happensAfter(other.id);
+    }
 }
---
+++ src/main/java/main/UpdateList.java
@@ -0,0 +1,50 @@
+package main;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author alex
+ */
+public class UpdateList {
+    List<Update> list;
+    
+    public UpdateList() {
+        list = new ArrayList<>();
+    }
+    
+    public UpdateList(List<Update> u) {
+        list = new ArrayList<>(u);
+    }
+    
+    public void add(Update u) {
+        for (int i = 0; i < list.size(); i++) {
+            if (u.id.happensBefore(list.get(i).id)) {
+                list.add(i, u);
+                break;
+            }
+        }
+    }
+    
+    public Update remove(Update u) {
+        return remove(u.id);
+    }
+    
+    public Update remove(UpdateID id) {
+        for (int i = 0; i < list.size(); i++)
+            if (list.get(i).id == id)
+                return list.remove(i);
+        return null;
+    }
+    
+    public Update getMostRecent() {
+        if (list.isEmpty())
+            return null;
+        return list.get(0);
+    }
+    
+    public UpdateList duplicate() {
+        return new UpdateList();
+    }
+}
